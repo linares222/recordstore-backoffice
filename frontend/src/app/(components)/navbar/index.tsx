da4bf7bfb0dc2f508473c7a9346d7064a722acd4
linares222 <tiagolinares.contacto@gmail.com>
2024-08-06 22:19:44 +0100

    sidebar and navbar hardcoded implementation

---
+++ frontend/src/app/(components)/navbar/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode } from "@/state";
+import { Avatar } from "@mui/material";
+import { Bell, Moon, SearchIcon, Settings, Sun } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const dispatch = useAppDispatch();
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
+
+  return (
+    <div className="flex justify-between w-full items-center space-x-2 pl-20 md:pr-4 md:pl-0">
+      <div className="flex items-center w-full space-x-4 ">
+        <form
+          action=""
+          className="max-w-xs bg-white h-14 border-2 rounded-lg flex items-center space-x-1 p-2 flex-1 mx-2 md:max-w-lg"
+        >
+          <SearchIcon size={20} className="text-gray-500" />
+          <input
+            className="bg-transparent flex-1 outline-none"
+            type="text"
+            placeholder="Search for a record"
+          />
+        </form>
+      </div>
+      <div className="flex items-center space-x-4 ">
+        <button className="hidden sm:block" onClick={toggleDarkMode}>
+          {isDarkMode ? <Moon size={28} /> : <Sun size={28} />}
+        </button>
+        <button className="hidden sm:block relative">
+          <Bell size={28} />
+          <span className="absolute -top-1 -right-0.5 bg-blue-500 text-xs text-white rounded-full h-4 w-4 flex items-center justify-center">
+            2
+          </span>
+        </button>
+
+        <div className="hidden sm:block w-[1px] bg-slate-400 h-8 mx-2" />
+        <button>
+          <Avatar alt="User profile picture" src="" />
+        </button>
+        <button className="hidden sm:block">
+          <Settings size={28} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/app/PageWrapper.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useEffect } from "react";
+import Navbar from "./(components)/navbar";
+import Sidebar from "./(components)/sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+
+type PageWrapperProps = {
+  children: React.ReactNode;
+};
+
+const PageWrapperLayout = ({ children }: PageWrapperProps) => {
+  const isSidebarOpen = useAppSelector((state) => state.global.isSidebarOpen);
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    //control the html elements without making it client side
+    isDarkMode
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.add("light");
+  });
+
+  return (
+    <div
+      className={`flex min-h-screen ${
+        isDarkMode ? "dark" : "light"
+      } bg-gray-50 text-gray-900 `}
+    >
+      <Sidebar />
+      <main
+        className={`flex flex-col w-full h-full bg-gray-50 px-9 py-7 ${
+          isSidebarOpen ? "md:pl-72" : "md:pl-24"
+        }`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+const PageWrapper = ({ children }: PageWrapperProps) => {
+  return (
+    <StoreProvider>
+      <PageWrapperLayout>{children}</PageWrapperLayout>
+    </StoreProvider>
+  );
+};
+
+export default PageWrapper;

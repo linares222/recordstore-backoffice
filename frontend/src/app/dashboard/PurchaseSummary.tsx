154269f8cd2a2a61777bcf5f80376fa2bc3bba35
linares222 <tiagolinares.contacto@gmail.com>
2024-08-18 15:14:51 +0100

    orders summary component

--- frontend/src/app/dashboard/PurchaseSummary.tsx
+++ frontend/src/app/dashboard/PurchaseSummary.tsx
@@ -17,10 +17,11 @@ type Props = {};
 const SaleSummary = (props: Props) => {
   const { data: dashboardData, isLoading } = useGetDashboardDataQuery();
   const PurchaseSummaryData = dashboardData?.purchaseSummary || [];
-  const lastDataPoint = PurchaseSummaryData[PurchaseSummaryData.length - 1] || null;
+  const lastDataPoint =
+    PurchaseSummaryData[PurchaseSummaryData.length - 1] || null;
 
   return (
-    <div className="flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
+    <div className="flex flex-col justify-between h-full row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
       {isLoading ? (
         <div className="m-5">
           <CircularProgress color="inherit" />
@@ -29,18 +30,20 @@ const SaleSummary = (props: Props) => {
         <>
           <div>
             <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
-              Purchase Summary
+              Order Summary
             </h2>
             <hr />
           </div>
 
-          <div>
+          <div className="flex flex-col flex-grow">
             <div className="mb-4 mt-7 px-7">
-              <p className="text-xs text-gray-400">Purchased</p>
+              <p className="text-xs text-gray-400">Sold</p>
               <div className="flex items-center">
                 <p className="text-2xl font-bold">
                   {lastDataPoint
-                    ? `${numeral(lastDataPoint.totalValue).format("0,0.00[0]")}€`
+                    ? `${numeral(lastDataPoint.totalValue).format(
+                        "0,0.00[0]"
+                      )}€`
                     : "0"}
                 </p>
                 {lastDataPoint && (
@@ -61,34 +64,37 @@ const SaleSummary = (props: Props) => {
                 )}
               </div>
             </div>
-            {/* CHART */}
-            <ResponsiveContainer width="100%" height={200} className="p-2">
-              <AreaChart
-                data={PurchaseSummaryData}
-                margin={{ top: 0, right: 0, left: -50, bottom: 30 }}
-              >
-                <XAxis dataKey="date" tick={false} axisLine={false} />
-                <YAxis tickLine={false} tick={false} axisLine={false} />
-                <Tooltip
-                  formatter={(value: number) => [`€${value.toLocaleString("en")}`]}
-                  labelFormatter={(label) => {
-                    const date = new Date(label);
-                    return date.toLocaleDateString("en-US", {
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                    });
-                  }}
-                />
-                <Area
-                  type="linear"
-                  dataKey="totalValue"
-                  stroke="#8884d8"
-                  fill="#8884d8"
-                  dot={true}
-                />
-              </AreaChart>
-            </ResponsiveContainer>
+            <div className="flex-grow">
+              <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                  data={PurchaseSummaryData}
+                  margin={{ top: 0, right: 30, left: -30, bottom: 0 }}
+                >
+                  <XAxis dataKey="date" tick={false} axisLine={false} />
+                  <YAxis tickLine={false} tick={false} axisLine={false} />
+                  <Tooltip
+                    formatter={(value: number) => [
+                      `€${value.toLocaleString("en")}`,
+                    ]}
+                    labelFormatter={(label) => {
+                      const date = new Date(label);
+                      return date.toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      });
+                    }}
+                  />
+                  <Area
+                    type="linear"
+                    dataKey="totalValue"
+                    stroke="#8884d8"
+                    fill="#8884d8"
+                    dot={true}
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            </div>
           </div>
         </>
       )}
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { useGetDashboardDataQuery } from "@/state/api";
 import React from "react";
 import PopularProducts from "./PopularProducts";
 import PurchaseSummary from "./PurchaseSummary";
+import OrderSummary from "./OrderSummary";
 
 const Dashboard = () => {
   const { data, error, isLoading } = useGetDashboardDataQuery();
@@ -10,6 +11,7 @@ const Dashboard = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <PopularProducts />
       <div className="row-span-3 xl:row-span-6 bg-gray-500"></div>
+      <OrderSummary/>
       <PurchaseSummary />
       <div className="row-span-3 bg-gray-500"></div>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
